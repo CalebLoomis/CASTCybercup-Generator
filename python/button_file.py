fdf60b16bc97e062ea94dd0b32e370c4093482db
Caleb Loomis <cloomis1@saisd.net>
2019-12-10 16:44:45 -0600

    python/ : Deleted extra file and directory

--- python/button_file.py
+++
@@ -1,7 +0,0 @@
-#This file takes global vars from GuiHandler
-
-filename = tkinterobj.file_selection_box()
-
-active_key[row][col + 1]['text'] = filename
-print (filename)
-print (active_key[row][col + 1]['text'])
